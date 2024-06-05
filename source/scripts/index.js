25c8b7726c1eaaccc5df0d3e464df4e67957e776
nastyaFR <146125731+nastyaFR@users.noreply.github.com>
2024-06-05 09:09:02 +0300

    Добавляет адаптивные декоративные элементы (#13)
    * Создает разметку всех страниц
    
    * Исправляет код по линтерам
    
    * Исправляет и дополняет разметку
    
    * Исправляет верстку по линтерам
    
    * Добавляет фавиконку и адаптивную графику
    
    * Добавляет базовую стилизацию с помощью препроцессора
    
    * Исправляет замечания по линтерам
    
    * Удаляет лишние препроцессоры
    
    * Добавляет векторную графику
    
    * Добавляет векторную графику
    
    * Добавляет бэкграунд
    
    * Добавляет адаптивные сетки
    
    * Добавляет адаптивные сетки
    
    * Исправляет линтеры
    
    * Исправляет линтеры
    
    * Выявляет ошибку в программе
    
    * Добавляет адаптивные декоративные эллементы
    
    * Добавляет декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const burgerButton = document.querySelector ('.main-nav__toggle');
+const closeButton = document.querySelector ('.main-nav__close-button');
+const navList = document.querySelector ('.main-nav__list');
+
+burgerButton.onclick = function () {
+  burgerButton.classList.add ('main-nav__toggle--special');
+  closeButton.classList.remove ('main-nav__close-button--special');
+  navList.classList.remove ('main-nav__list--hide');
+};
+closeButton.onclick = function () {
+  closeButton.classList.add ('main-nav__close-button--special');
+  burgerButton.classList.remove ('main-nav__toggle--special');
+  navList.classList.add ('main-nav__list--hide');
+};
